c72ea9546a0e4a66f22a4c7ca0b547d220e630ad
Staryo40 <arrrryow@gmail.com>
2025-04-02 02:44:04 +0700

    feat: refactor boxBorder to TopLeft and adjust colorMean to it

--- src/Quadtree/QuadtreeNode.cs
+++ src/Quadtree/QuadtreeNode.cs
@@ -7,7 +7,7 @@ namespace Quadtree{
     class QuadtreeNode
     {
         private QuadtreeTree tree;
-        public (int, int) TopLeft { get; private set; }
+        public (int, int) TopLeft { get; private set; } // top, left
         public int Depth { get; private set; }
         public int Width { get; private set; }
         public int Height { get; private set; }
@@ -23,16 +23,16 @@ namespace Quadtree{
             IsLeaf = false;
         }
 
-        public QuadtreeNode(QuadtreeTree t, (int, int, int, int) bor, int d, (QuadtreeNode, QuadtreeNode, QuadtreeNode, QuadtreeNode)? c = null)
+        public QuadtreeNode(QuadtreeTree t, (int, int) tl, int d, int w, int h, (QuadtreeNode, QuadtreeNode, QuadtreeNode, QuadtreeNode)? c = null)
         {
             tree = t;
             Depth = d;
             Children = c;
 
-            var (top, left, right, bottom) = bor;
-            TopLeft = (left, top);
-            Width = right - left;
-            Height = bottom - top;  
+            var (top, left) = tl;
+            TopLeft = (top, left);
+            Width = w;
+            Height = h;  
             IsLeaf = false;
         }
 
@@ -45,13 +45,20 @@ namespace Quadtree{
 
         public void split(){
             var (top, left, right, bottom) = GetBorders();
-            int midHor = left + (right - left) / 2;
-            int midVer = top + (bottom - top) / 2;
+            int midHor = top + (bottom - top)/ 2;
+            int midVer = left + (right - left) / 2;
+
+            int widthLeft = (right - left) / 2;  
+            int widthRight = (right - left) - widthLeft;  
+
+            int heightTop = (bottom - top) / 2;  
+            int heightBottom = (bottom - top) - heightTop;  
             
-            QuadtreeNode topLeft = new QuadtreeNode(tree, (top, left, midVer, midHor), Depth + 1, null);
-            QuadtreeNode topRight = new QuadtreeNode(tree, (top, midVer, right, midHor), Depth + 1, null);
-            QuadtreeNode bottomLeft = new QuadtreeNode(tree, (midHor, left, midVer, bottom), Depth + 1, null);
-            QuadtreeNode bottomRight = new QuadtreeNode(tree, (midHor, midVer, right, bottom), Depth + 1, null);
+            QuadtreeNode topLeft = new QuadtreeNode(tree, (top, left), Depth + 1, widthLeft, heightTop, null);
+            QuadtreeNode topRight = new QuadtreeNode(tree, (top, midVer), Depth + 1, widthRight, heightTop, null);
+            QuadtreeNode bottomLeft = new QuadtreeNode(tree, (midHor, left), Depth + 1, widthLeft, heightBottom, null);
+            QuadtreeNode bottomRight = new QuadtreeNode(tree, (midHor, midVer), Depth + 1, widthRight, heightBottom, null);
+
             Children = (topLeft, topRight, bottomLeft, bottomRight);
         }
 
@@ -63,8 +70,8 @@ namespace Quadtree{
             double varianceG = 0;
             double varianceB = 0;
             
-            for (int i = 0; i < Height; i++){
-                for (int j = 0; j < Width; j++){
+            for (int i = 0; i < Width; i++){
+                for (int j = 0; j < Height; j++){
                     varianceR += (tree.GetPixel(i, j, TopLeft).R - meanR) * (tree.GetPixel(i, j, TopLeft).R - meanR);
                     varianceG += (tree.GetPixel(i, j, TopLeft).G - meanG) * (tree.GetPixel(i, j, TopLeft).G - meanG);
                     varianceB += (tree.GetPixel(i, j, TopLeft).B - meanB) * (tree.GetPixel(i, j, TopLeft).B - meanB);
@@ -84,8 +91,10 @@ namespace Quadtree{
             double sumR = 0;
             double sumG = 0;
             double sumB = 0;
-            for (int i = 0; i < Height; i++){
-                for (int j = 0; j < Width; j++){
+
+            Console.WriteLine($"Image Dimensions in mean: {Width}x{Height}, TopLeft: {TopLeft.Item2}x{TopLeft.Item1}, Max iteration: {TopLeft.Item2 + Width}x{TopLeft.Item1 + Height}");
+            for (int i = 0; i < Width; i++){
+                for (int j = 0; j < Height; j++){
                     sumR += tree.GetPixel(i, j, TopLeft).R;
                     sumG += tree.GetPixel(i, j, TopLeft).G;
                     sumB += tree.GetPixel(i, j, TopLeft).B;
