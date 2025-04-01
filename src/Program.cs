455521a516ece02a465fbb0aa9e167c042b85a9b
Rafizan <163702740+Rafizan46@users.noreply.github.com>
2025-04-01 19:16:21 +0700

    feat: add initial I/O

---
+++ src/Program.cs
@@ -0,0 +1,34 @@
+﻿using IOHandler;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+
+class Program
+{
+    static void Main()
+    {
+        #region inputs
+        Color[,] image = InputHandler.GetImage();
+
+        int errorMethod = InputHandler.GetErrorMethod();
+
+        float treshold = InputHandler.GetTreshold();
+
+        int minimumBlock = InputHandler.GetMinimumBlock();
+
+        float targetCompression = InputHandler.GetTargetCompression();
+
+        string imageOutputPath = InputHandler.GetOutputPath("Masukkan alamat absolut gambar hasil kompresi (.png): ", ".png");
+
+        string gifOutputPath = InputHandler.GetOutputPath("Masukkan alamat absolut gif hasil (.gif): ", ".gif");
+        #endregion
+
+        #region processing
+        Color[,] resultImage = image;
+        #endregion
+
+        #region outputs
+        OutputHandler.SaveImage(imageOutputPath, resultImage);
+        #endregion
+    }
+}
