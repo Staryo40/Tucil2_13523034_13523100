56d091ecd10d943ef3df1f645abc1db043c85235
Staryo40 <arrrryow@gmail.com>
2025-04-07 22:20:29 +0700

    feat: new QuadtreeArray structure to make GIF

---
+++ src/Quadtree/QuadtreeArray.cs
@@ -0,0 +1,80 @@
+using System;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.Formats.Jpeg;
+
+namespace Quadtree{
+    class QuadtreeArray{
+        public Rgba32[,] OriginalImage { get; private set; } 
+        public long OriginalSize { get; private set; }
+        public int Extension {get; private set; }   // 1. PNG
+                                                    // 2. JPG
+        public List<Rgba32[,]> Buffer { get; private set; }
+        public List<long> ExecutionTimes { get; private set; }
+        public List<float> CompressionRates { get; private set; }
+        public QuadtreeArray(Rgba32[,] i, long size, int extension){
+            this.OriginalImage = i;
+            this.OriginalSize = size;
+            this.Extension = extension;
+            this.Buffer = new List<Rgba32[,]>();
+            this.ExecutionTimes = new List<long>();
+            this.CompressionRates = new List<float>();
+        }
+        public void CreateGIFImages(){
+            for (int i = 0; i < 10; i++){
+                int imageWidth = OriginalImage.GetLength(0);
+                int imageHeight = OriginalImage.GetLength(1);
+                int minimalBlock = (int)((imageWidth / Math.Pow(2, i)) * (imageHeight / Math.Pow(2, i)));
+
+                long startTime = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+                QuadtreeTree t = new QuadtreeTree(OriginalImage, imageWidth, imageHeight, minimalBlock, 1, 0);
+                long endTime = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+
+                Rgba32[,] outputArray = t.CreateImage();
+                float compression = 1 - ((float) GetExpectedFileSize(outputArray, Extension) / (float) OriginalSize);
+                ExecutionTimes.Add(endTime-startTime);
+                Buffer.Add(outputArray);
+                CompressionRates.Add(compression);
+
+                Console.Write($"\rCurrently completed GIF images: {i + 1}/10");
+            }
+            Console.WriteLine("");
+        }
+        public static long GetExpectedFileSize(Rgba32[,] pixelMatrix, int extension)
+        {
+            using var image = MatrixToImage(pixelMatrix);
+            using var ms = new MemoryStream();
+
+            switch (extension)
+            {
+                case 1: // PNG
+                    image.Save(ms, new PngEncoder());
+                    break;
+                case 2: // JPG
+                    image.Save(ms, new JpegEncoder()); 
+                    break;
+                default:
+                    throw new ArgumentException("Unsupported extension value.");
+            }
+
+            return ms.Length;
+        }
+        private static Image<Rgba32> MatrixToImage(Rgba32[,] matrix)
+        {
+            int width = matrix.GetLength(0);
+            int height = matrix.GetLength(1);
+            var image = new Image<Rgba32>(width, height);
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    image[x, y] = matrix[x, y];
+                }
+            }
+
+            return image;
+        }
+    }
+}
