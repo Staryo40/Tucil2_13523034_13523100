5436807e7328ab9943938c9eaf8005e68fe684da
Staryo40 <arrrryow@gmail.com>
2025-04-10 16:31:25 +0700

    feat: final main program before target implementation

--- src/Program.cs
+++ src/Program.cs
@@ -10,35 +10,16 @@ class Program
     static void Main()
     {
         #region inputs
-        // Console.WriteLine("Image Dimension: " + image.GetLength(0) + "x" + image.GetLength(1));
-        // Console.WriteLine("Image Area: " + (image.GetLength(0) * image.GetLength(1)));
-
         (Rgba32[,] image, long oriFileSize) = InputHandler.GetImage();
-
-        Console.Clear();
-        InputHandler.ShowInputStatus();
-
-        // Start looping progress bar animation
-        CancellationTokenSource cts = new CancellationTokenSource();
-        var loadingTask = Task.Run(() => InputHandler.ShowLoopingProgressBar("Memproses input", cts.Token));
-
-        QuadtreeArray minMax = new QuadtreeArray(image, oriFileSize, 2);
-        minMax.CreateMinMaxImages();
-
-        // Stop animation
-        cts.Cancel();
-        loadingTask.Wait();
-
-        // Set thresholds
-        InputHandler.MinTargetThreshold = minMax.CompressionRates[0];
-        InputHandler.MaxTargetThreshold = minMax.CompressionRates[1];
-
-        Console.WriteLine(); 
-
         int errorMethod = InputHandler.GetErrorMethod();
-        double threshold = InputHandler.GetThreshold();
-        int minimumBlock = InputHandler.GetMinimumBlock();
         float targetCompression = InputHandler.GetTargetCompression();
+
+        int minimumBlock = 1;
+        double threshold = 0;
+        if (targetCompression == 0){
+            threshold = InputHandler.GetThreshold();
+            minimumBlock = InputHandler.GetMinimumBlock();
+        }
         string imageOutputPath = InputHandler.GetOutputPath("Masukkan alamat absolut gambar hasil kompresi: ", InputHandler.ImageExtensionType);
         string gifOutputPath = InputHandler.GetOutputPath("Masukkan alamat absolut gif hasil (.gif): ", ".gif");
         
@@ -52,36 +33,53 @@ class Program
         Console.WriteLine("   PROCESSING");
         Console.ResetColor();
 
-        // GIF Processing
-        // QuadtreeArray ta = new QuadtreeArray(image, oriFileSize, 2);
-        // ta.CreateGIFImages();
+        long startTimeImage;
+        long endTimeImage;
+        QuadtreeTree t;
+        Rgba32[,] outputArray;
+
+        if (targetCompression == 0){ // NO compression target
+            // Image processing
+            startTimeImage = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+
+            t = new QuadtreeTree(image, image.GetLength(0), image.GetLength(1), minimumBlock, errorMethod, threshold);
+            outputArray = t.CreateImage();
+
+            endTimeImage = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+        } else { // Compression target
+            // Image Processing
+            Console.WriteLine("Pemrosesan image akan lebih lama dengan fitur target compression");
+
+            startTimeImage = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+
+            // Binary Search get error 
+            t = new QuadtreeTree(image, image.GetLength(0), image.GetLength(1), 1, errorMethod, threshold); // change threshold with the one got from binary search
+            outputArray = t.CreateImage();
+
+            endTimeImage = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+        }
+        // GIF processing
+        QuadtreeArray ta = new QuadtreeArray(image, oriFileSize, InputHandler.getImageExtensionNum());
+        ta.CreateGIFImages(t);
         
-        // long gifExecutionTime = 0;
-        // for (int i = 0; i < ta.ExecutionTimes.Count; i++){
-        //     gifExecutionTime += ta.ExecutionTimes[i];
-        // }
+        long gifExecutionTime = 0;
+        for (int i = 0; i < ta.ExecutionTimes.Count; i++){
+            gifExecutionTime += ta.ExecutionTimes[i];
+        }
+        Console.WriteLine();
 
-        // Image Processing
-        long startTimeImage = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
-
-        QuadtreeTree t = new QuadtreeTree(image, image.GetLength(0), image.GetLength(1), minimumBlock, errorMethod, threshold);
-        Rgba32[,] outputArray = t.CreateImageAtDepth(11);
-
-        long endTimeImage = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
         #endregion
 
         #region outputs
         Console.ForegroundColor = ConsoleColor.Cyan;
         Console.WriteLine("   OUTPUT");
         Console.ResetColor();
-
-        Console.WriteLine("Waktu eksekusi: " + (endTimeImage - startTimeImage) + " ms");
-        // string imageOutputPath = @"C:\Users\Aryo\PersonalMade\ITB Kuliah Semesteran\Semester 4\Strategi Algoritma\Tucil-Tubes 2025\Tucil2_13523034_13523100\src\output.jpg";
-        // string gifOutputPath = @"C:\Users\Aryo\PersonalMade\ITB Kuliah Semesteran\Semester 4\Strategi Algoritma\Tucil-Tubes 2025\Tucil2_13523034_13523100\src\output.gif";
-
+        
         OutputHandler.SaveImage(imageOutputPath, outputArray);
-        // OutputHandler.SaveGIF(gifOutputPath, ta.Buffer);
+        OutputHandler.SaveGIF(gifOutputPath, ta.Buffer);
 
+        Console.WriteLine("Waktu eksekusi gambar: " + (endTimeImage - startTimeImage) + " ms");
+        Console.WriteLine("Waktu eksekusi GIF: " + (gifExecutionTime) + " ms");
         Console.WriteLine("Kedalaman Pohon: " + t.maxDepth);
         Console.WriteLine("Jumlah Simpul: " + t.nodeCount);
         Console.WriteLine("Jumlah Daun: " + t.leafCount);
@@ -92,13 +90,10 @@ class Program
         Console.WriteLine("Ukuran file gambar setelah kompresi: " + compFileSize + " bytes");
 
         float compPercentage = (1 - (float) compFileSize / (float) oriFileSize) * 100f;
+        if (targetCompression != 0){
+            Console.WriteLine("Target kompresi: " + targetCompression + "%");
+        }
         Console.WriteLine("Persentase kompresi: " + compPercentage + "%");
-
-        // Console.WriteLine("Waktu eksekusi buat GIF: " + gifExecutionTime + " ms");
-        // for (int i = 0; i < ta.CompressionRates.Count; i++)
-        // {
-        //     Console.WriteLine($"compression {i} = {ta.CompressionRates[i]}");
-        // }
 
         #endregion
     }
